443ba2fb4e90c4842934449581279d258f32981a
Dora <doradora.prog@gmail.com>
2017-09-25 00:41:26 +0900

    ファイルの追加＆READMEの加筆

---
+++ LinqPractice/LinqPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 94, 5 };
+
+
+            var squareArr = arr.Select(value => value * value);
+            // { 4, 9, 16, 25, 36, 49, 64, 81, 100, 169, 8836, 25 }
+            // Selectは写像。メンバに一つずつ関数を適用し、その戻り値で新しい配列（コレクション）を作る。
+            // このとき、各値を2乗した配列を返す。
+
+            var oddArr = arr.Where(value => value % 2 == 1);
+            // { 3, 5, 7, 9, 13, 5 }
+            // Whereはフィルター。メンバの中から条件に合う要素を取り出す。
+            // このとき、奇数の値のみを抽出した配列を返す。
+
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # LINQ
 LINQ(C#)を勉強しようと思い立った
+
+勉強の中で得たLINQについての情報を書いていきます。
+
+## About LINQ  
+
+* C#3.0で登場した、リレーショナルデータベースやXMLに対する動作をプログラミング言語に統合するもの。
+* Language INtegrated Queryの略
+* 様々なタイプのデータソースに対する処理を簡単・簡潔に行える
+* C#でSQLライクなデータベース操作構文を組み込むライブラリ
+* [福岡のアイドルグループ](http://loveinq.com/)ではない
+
+
+
+## 参考文献
+
+* [LINQ - C# によるプログラミング入門|++C++; // 未確認飛行 C](http://ufcpp.net/study/csharp/sp3_linq.html)
+* [C# やるなら LINQ を使おう](http://yohshiy.blog.fc2.com/blog-entry-274.html)
