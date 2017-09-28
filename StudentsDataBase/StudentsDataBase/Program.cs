611cbad2c15466d556061a8524cb108e6e948121
Dora <doradora.prog@gmail.com>
2017-09-28 13:55:06 +0900

    Add Student Class

--- StudentsDataBase/StudentsDataBase/Program.cs
+++ StudentsDataBase/StudentsDataBase/Program.cs
@@ -10,6 +10,71 @@ namespace StudentsDataBase
     {
         static void Main(string[] args)
         {
+            
+        }
+    }
+
+    class Students
+    {
+        private int No; // 学生番号
+        private string Nam; // 名前
+        private string Dept; // 学科
+        private string Facu; // 学部
+
+        public int StudentNo
+        {
+            get
+            {
+                return No;
+            }
+            set
+            {
+                No = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return Nam;
+            }
+            set
+            {
+                Nam = value;
+            }
+        }
+
+        public string Department
+        {
+            get
+            {
+                return Dept;
+            }
+            set
+            {
+                Dept = value;
+            }
+        }
+
+        public string Faculty
+        {
+            get
+            {
+                return Facu;
+            }
+            set
+            {
+                Facu = value;
+            }
+        }
+
+        public Students(int no, string na, string de, string fa)
+        {
+            No = no;
+            Nam = na;
+            Dept = de;
+            Facu = fa;
         }
     }
 }
