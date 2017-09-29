3568cd33bccb4d7e945402a3d84bbed595f69e83
Dora <doradora.prog@gmail.com>
2017-09-30 02:11:12 +0900

    Extend StudentDataBase

--- StudentsDataBase/StudentsDataBase/Program.cs
+++ StudentsDataBase/StudentsDataBase/Program.cs
@@ -10,15 +10,45 @@ namespace StudentsDataBase
     {
         static void Main(string[] args)
         {
-            
+            // データをリストに格納。一旦は初期値で入れてるけど、何かしらで追加等できるようにする。
+            // データは全て架空で意味は無いです。ほんとです。
+            List<Student> Students = new List<Student>()
+            {
+                new Student(140101,"Shizuka Mogami","Literature"),
+                new Student(120201,"Anastasia","Literature"),
+                new Student(130301,"Mizuki Makabe","Economics"),
+                new Student(150301,"Yayoi Takatsuki","Economics"),
+                new Student(100301,"Chihaya Kisaragi","Law"),
+                new Student(021301,"Ritsuko Akiduki","Science"),
+                new Student(100401,"Haruka Amami","Education"),
+                new Student(163401,"Loco","Design"),
+                new Student(121301,"Eri Mizutani","Science"),
+                new Student(100401,"Ami Futami","Education"),
+                new Student(100402,"Mami Futami","Education"),
+            };
+
+            foreach(Student s in Students)
+            {
+                s.Write();
+            }
+
+            Console.WriteLine("\n");
+
+
+            // ID順に並び替え
+            var IDSort = Students.OrderBy(student => student.StudentNo);
+
+            foreach(Student s in IDSort)
+            {
+                s.Write();
+            }
         }
     }
 
-    class Students
+    class Student
     {
         private int No; // 学生番号
         private string Nam; // 名前
-        private string Dept; // 学科
         private string Facu; // 学部
 
         public int StudentNo
@@ -44,19 +74,7 @@ namespace StudentsDataBase
                 Nam = value;
             }
         }
-
-        public string Department
-        {
-            get
-            {
-                return Dept;
-            }
-            set
-            {
-                Dept = value;
-            }
-        }
-
+        
         public string Faculty
         {
             get
@@ -69,12 +87,16 @@ namespace StudentsDataBase
             }
         }
 
-        public Students(int no, string na, string de, string fa)
+        public Student(int no, string na, string fa)
         {
             No = no;
             Nam = na;
-            Dept = de;
             Facu = fa;
+        }
+
+        public void Write()
+        {
+            Console.WriteLine($"ID:{No} {Name} ( {Facu} Faculty )");
         }
     }
 }
