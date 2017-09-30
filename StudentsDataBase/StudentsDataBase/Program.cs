aff15c210e50b6392379634490349667707eb8a9
Dora <doradora.prog@gmail.com>
2017-09-30 13:41:25 +0900

    Add Additional features of datas & Browsing function of datas

--- StudentsDataBase/StudentsDataBase/Program.cs
+++ StudentsDataBase/StudentsDataBase/Program.cs
@@ -8,11 +8,12 @@ namespace StudentsDataBase
 {
     class Program
     {
+        static List<Student> Students;
         static void Main(string[] args)
         {
             // データをリストに格納。一旦は初期値で入れてるけど、何かしらで追加等できるようにする。
             // データは全て架空で意味は無いです。ほんとです。
-            List<Student> Students = new List<Student>()
+            Students = new List<Student>()
             {
                 new Student(140101,"Shizuka Mogami","Literature"),
                 new Student(120201,"Anastasia","Literature"),
@@ -26,6 +27,30 @@ namespace StudentsDataBase
                 new Student(100401,"Ami Futami","Education"),
                 new Student(100402,"Mami Futami","Education"),
             };
+
+            int command = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("次のうちから行いたいことを数字で選んでください");
+                Console.Write("1:新規登録　2:閲覧　9:終了 > ");
+                if(int.TryParse(Console.ReadLine(),out int c))
+                {
+                    switch(c)
+                    {
+                        case 1:
+                            Write();
+                            break;
+                        case 2:
+                            Read();
+                            break;
+                        case 9:
+                            break;
+                    }
+                    if (c == 9) break;
+                }
+            }
+            /*
 
             foreach(Student s in Students)
             {
@@ -41,6 +66,104 @@ namespace StudentsDataBase
             foreach(Student s in IDSort)
             {
                 s.Write();
+            }
+
+            */
+        }
+
+        static void Write()
+        {
+            int no;
+            string nam = "";
+            string Facu = "";
+            while(true)
+            {
+                Console.Clear();
+                Console.Write("学生番号を入力してください > ");
+                if(int.TryParse(Console.ReadLine(),out no))
+                {
+                    bool IsIdOk = true;
+                    foreach(Student s in Students)
+                    {
+                        if(no == s.StudentNo)
+                        {
+                            IsIdOk = false;
+                            break;
+                        }
+                    }
+                    if(IsIdOk) break;
+                    else
+                    {
+                        Console.WriteLine("すでに存在するIDです\n");
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("無効な数値です\n");
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("名前を入力してください > ");
+                nam = Console.ReadLine();
+                if(nam !="")
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("学部を入力してください > ");
+                Facu = Console.ReadLine();
+                if (Facu != "")
+                {
+                    break;
+                }
+            }
+
+            Students.Add(new Student(no, nam, Facu));
+        }
+
+        static void Read()
+        {
+            var displayStudents = Students.OrderBy(stu => stu.StudentNo);
+            int n = 1;
+            while(true)
+            {
+                Console.Clear();
+                foreach(Student s in displayStudents)
+                {
+                    s.Write();
+                }
+                while (true)
+                {
+                    Console.WriteLine("\nソート順を変更する／終了する");
+                    Console.Write("1:番号順 2:名前順 3:学部順 9:終了 > ");
+                    if(int.TryParse(Console.ReadLine(),out n))
+                    {
+                        if (n > 0 && n < 5 || n == 9) 
+                        {
+                            break;
+                        }
+                    }
+                }
+                switch(n)
+                {
+                    case 1:
+                        displayStudents = displayStudents.OrderBy(st => st.StudentNo);
+                        break;
+                    case 2:
+                        displayStudents = displayStudents.OrderBy(st => st.Name);
+                        break;
+                    case 3:
+                        displayStudents = displayStudents.OrderBy(st => st.Faculty);
+                        break;
+                    case 9:
+                        return;
+                }
             }
         }
     }
